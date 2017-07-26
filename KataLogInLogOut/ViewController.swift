ffa1bc7f3118d188098feca7c74b4e1892b0ad9f
nasli <nalvarez@solidgear.es>
2017-07-26 13:12:40 +0200

    Add folders

---
+++ KataLogInLogOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KataLogInLogOut
+//
+//  Created by Noelia Alvarez on 26/07/2017.
+//  Copyright © 2017 Noelia Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
